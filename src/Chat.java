55ca9312e5611cddb624b8f96fe92700cf4d9a66
stealthcamaro <lujanuriel@icloud.com>
2024-10-14 10:10:20 -0500

    cleaning up repository by removing src, and tutorial folders.

--- src/Chat.java
+++
@@ -1,7 +0,0 @@
-package src;
-public class Chat {
-    private String chat_tittle; //name or title of the chat
-    private Message messages;  // linked list starting with most recent message
-    private int[] students; //student emails part of the chatgroup
-
-}
