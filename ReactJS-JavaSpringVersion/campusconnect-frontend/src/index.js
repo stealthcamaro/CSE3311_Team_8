dcb2d3c7e2865d929a8abad3d33745795df4ef30
LuisEDelRio <luis.delriocarrillo@mavs.uta.edu>
2024-10-31 19:06:45 -0500

    RegistrationTesting

--- ReactJS-JavaSpringVersion/campusconnect-frontend/src/index.js
+++ ReactJS-JavaSpringVersion/campusconnect-frontend/src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import RegistrationPage from './components/RegistrationPage';
+import { BrowserRouter } from 'react-router-dom';
+import RegistrationPage from './components/RegistrationPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <RegistrationPage/> */}
-    <App />
+    <BrowserRouter>
+      {/* <App/> */}
+      <RegistrationPage />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
